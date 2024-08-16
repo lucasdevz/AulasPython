db709ef2c76275652df4db58f2063ad3f3387635
lucas <85170796+lucasdevz@users.noreply.github.com>
2024-08-16 18:14:15 +0000

    Comandos iniciais

---
+++ 1.Tipos primitivos/Tipagem dinamica.py
@@ -0,0 +1,14 @@
+import os
+
+os.system("cls|| clear") #Limpa o terminal
+
+
+#Tipagem Dinâmica.
+
+nome = "Lucas"
+idade = "20"
+altura = "1.70"
+
+print(f"Nome:  + {nome}")
+print(F"Idade: + {idade}")
+print(f"altura: + {altura}")                   
---
+++ 1.Tipos primitivos/Tipagem estatica.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("cls|| clear") #Limpa o terminal
+
+#Tipagem estática.
+nome : str # cadeia
+idade : int # inteiro
+altura : float # real
+
+nome = "Lucas"
+idade = 20
+altura = 1,70
+
+print(f"Nome:  + {nome}")
+print(F"Idade: + {idade}")
+print(f"altura: + {altura}")
