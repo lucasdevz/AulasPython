ff625effc932618a4670cd8a111450df66f808da
lucas <85170796+lucasdevz@users.noreply.github.com>
2024-08-30 19:56:53 +0000

    Aula30.08

--- 4. While/1.10.RepetirNotas.py
+++ 4. While/1.10.RepetirNotas.py
@@ -1,24 +1,59 @@
+"""
+Escreva um algoritmo que pergunte ao usuário se deseja inserir mais uma nota.
+Mostre um menu conforme o descrito abaixo:
+
+S – Inserir mais uma nota;
+P – Ver quantas notas foram inseridas;
+N – Calcular a média aritmética das notas informadas.
+
+O programa deve mostrar a média aritmética para o usuário.
+
+"""
+
 import os
-os.system("cls || clear")
+import time
 
-soma = 0
-contador = 0
+os.system("clear")
 
-while True:
-    nota = float(input("Digite sua nota: "))
-    contador +=1
-    soma += nota
+soma : float = 0
+quantidadeNotas = 0
 
-    resposta = input("Deseja inserir mais uma nota? ").upper()
-    # resposta = resposta.upper() # Converter Maiusculo
+while True :
 
-    if resposta =="N":
-        break
+    print("""
+        \t=== MENU ===
+        S - Inserir uma nota
+        P - Ver quantas notas foram inseridas
+        N - Calcular média aritmética
+    """)
+   
+    resposta = input("Deseja inserir uma nota: ").upper()
+   
+    match  resposta:
+        case "S":
+            nota = float(input("\nDigite uma nota: "))
+            soma += nota
+            quantidadeNotas += 1
 
-    resposta2 = input("Deseja ver quantas notas foram inseridas? ").upper()
-    if resposta2 =="N":
-        break
+        case "P":
+            if quantidadeNotas == 0:
+                print("Não foram inseridas notas. \n")            
+                input("Pressione uma tecla para continuar...")
+                time.sleep(3)
+            else:
+                print(f"Quantidade de notas inseridas: {quantidadeNotas} \n")
+                input("Pressione uma tecla para continuar...")
+               
+        case "N":
+            if quantidadeNotas == 0:
+                print("Não foram inseridas notas. \n")
+            else:
+                break
 
+        case _:
+            print("Opção inválida... \n")
+            time.sleep(3)
 
-media = soma /contador
+media  = soma / quantidadeNotas
+
 print(f"Média: {media}")
