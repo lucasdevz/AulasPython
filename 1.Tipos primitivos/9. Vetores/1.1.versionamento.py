414a49bda9d8abf10fdd76689e89f4204caba27d
lucas <85170796+lucasdevz@users.noreply.github.com>
2024-09-25 17:13:10 +0000

    aulahj

--- 1.Tipos primitivos/9. Vetores/1.1.versionamento.py
+++ 1.Tipos primitivos/9. Vetores/1.1.versionamento.py
@@ -1,3 +1,20 @@
+"""
+# Identificar arquivos para salvar (commit)
+git status
+
+# Adicionar arquivos para salvar (commit)
+git add .
+
+# Realizando salvamento (commit)
+git commit -m "mensagem"
+
+# Atualiza/traz seu repositório local em relação a repositório remoto.
+git pull
+
+# Envia seu código para o repositório remoto(github)
+
+"""
+
 import os
 os.system("cls || clear")
 
---
+++ Atividades_Pontuada/atividadepontuada.py
@@ -0,0 +1,21 @@
+import os
+os.system("cls||clear")
+
+print("""
+ℝ𝕖𝕤𝕥𝕒𝕦𝕣𝕒𝕟𝕥𝕖 𝕤𝕖𝕟𝕒𝕚
+""")
+
+print('1. Pizza')
+print('2. Macarrão')
+print('3. Feijoada')
+print('4. Suchi')
+print('5. Salada')
+print('6. Churrasco')
+print('7. Pastel\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
+
+
+
+
